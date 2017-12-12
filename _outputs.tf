f819b48e5d6f0eecd84b737c70d7e829cf10d8a5
Thomas Butler <tombutler.development@gmail.com>
2017-12-12 13:39:23 +1100

    Output information required to start an ecs service

--- _outputs.tf
+++ _outputs.tf
@@ -1,3 +1,15 @@
 output "alb_dns_name" {
   value = "${module.load_balancer.alb_dns_name}"
 }
+
+output "task_role_arn" {
+  value = "${module.ecs_policy.role_arn}"
+}
+
+output "target_group_arn" {
+  value = "${module.load_balancer.alb_target_group}"
+}
+
+output "lb_role" {
+  value = "/ecs/${module.public.ecs_lb_role}"
+}
